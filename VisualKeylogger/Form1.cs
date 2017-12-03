690b488a6ed39e9f39ee90eea495dfa80a5411b0
Kristupas Stumbrys <Kristupas.stumbrys@gmail.com>
2017-12-03 16:51:31 +0200

    Added MouseKeyHook package and implemented to to show the last key being pressed

--- VisualKeylogger/Form1.cs
+++ VisualKeylogger/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Gma.System.MouseKeyHook;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,25 @@ namespace VisualKeylogger
 {
     public partial class Form1 : Form
     {
+        private IKeyboardMouseEvents m_GlobalHook;
+
         public Form1()
         {
             InitializeComponent();
+
+            m_GlobalHook = Hook.GlobalEvents();
+            m_GlobalHook.KeyPress += GlobalHookKeyPress;
+        }
+
+        private void GlobalHookKeyPress(object sender, KeyPressEventArgs e)
+        {
+            label1.Text = String.Format("Last key pressed: \t{0}", e.KeyChar);
+        }
+
+        ~Form1()
+        {
+            m_GlobalHook.KeyPress -= GlobalHookKeyPress;
+            m_GlobalHook.Dispose();
         }
     }
 }
